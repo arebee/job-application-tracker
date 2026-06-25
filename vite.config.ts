b74a28b6dc771b1de2b5a56d344bddfa3f50630e
rburte <rburte@number8wire.com>
2026-06-25 13:32:43 -0700

    feat: add support for local TLS

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	ssr: {
 		external: ['better-sqlite3', 'bcrypt']
-	}
+	},
+	preview: {
+		allowedHosts: ['the-heart-of-gold.local', 'localhost', '127.0.0.1'],
+		// allowedHosts: true,
+		https: {
+			key: fs.readFileSync(path.resolve(path.join(__dirname, '../_mkcert/'), 'the-heart-of-gold.local-key.pem')),
+			cert: fs.readFileSync(path.resolve(path.join(__dirname, '../_mkcert/'), 'the-heart-of-gold.local.pem')),
+		},
+	},
 });
